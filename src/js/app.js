607ba2e759f5eb1216188c77ab9b6f8a29cf9a8a
pntbutter <frallanawsm@gmail.com>
2019-12-02 20:13:40 +0100

    ttt redesign

---
+++ src/js/app.js
@@ -0,0 +1,16 @@
+// Icons
+const feather = require('feather-icons');
+feather.replace();
+
+
+// Slider
+import Swiper from 'swiper';
+var mySwiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  loop: true,
+  speed: 1000,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  }
+});
